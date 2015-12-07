b3ed93072726859b6cee1996e7e43293003d59e6
Jerome Terry <jerome.l.terry@gmail.com>
2015-12-07 13:04:04 -0330

    Adding route to get download url

--- main.js
+++ main.js
@@ -41,14 +41,36 @@ router.get('/files/uploadurl', function(req, res) {
         ContentType: req.query.file_type,
         ACL: 'public-read'
     };
-    s3.getSignedUrl('putObject', s3_params, function(err, data){
+    s3.getSignedUrl('putObject', s3_params, function(err, url){
         if(err) {
             console.log(err);
         }
         else {
             var return_data = {
-                signed_request: data,
-                url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.file_name
+                url: url
+            };
+            res.write(JSON.stringify(return_data));
+            res.end();
+        }
+    });
+});
+
+router.get('/files/downloadurl', function(req, res) {
+    var s3 = new aws.S3();
+    var s3_params = {
+        Bucket: S3_BUCKET,
+        Key: req.query.file_name,
+        Expires: 60,
+        ContentType: req.query.file_type,
+        ACL: 'public-read'
+    };
+    s3.getSignedUrl('getObject', s3_params, function(err, url){
+        if(err) {
+            console.log(err);
+        }
+        else {
+            var return_data = {
+                url: url
             };
             res.write(JSON.stringify(return_data));
             res.end();
